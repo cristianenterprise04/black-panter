5ea245003d0491cca1ca005295fd51681f74e3d7
cristianenterprise04 <52579073+cristianenterprise04@users.noreply.github.com>
2019-07-05 13:10:14 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
-
-        import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+########### Set up the chart
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[35, 60, 85, 75]
+abv_values=[5.4, 7.1, 9.2, 4.3]
+color1='lightblue'
+color2='darkgreen'
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+bitterness = go.Bar(
+    x=beers,
+    y=ibu_values,
+    name='IBU',
+    marker={'color':color1}
+)
+alcohol = go.Bar(
+    x=beers,
+    y=abv_values,
+    name='ABV',
+    marker={'color':color2}
+)
 
+beer_data = [bitterness, alcohol]
+beer_layout = go.Layout(
+    barmode='group',
+    title = 'Beer Comparison'
+)
+
+beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+########### Display the chart
+
+app = dash.Dash()
 server = app.server
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
+app.layout = html.Div(children=[
+    html.H1('Flying Dog Beers'),
+    dcc.Graph(
+        id='flyingdog',
+        figure=beer_fig
     ),
-    html.Div(id='display-value')
-])
-
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    html.A('Code on Github', href='https://github.com/austinlasseter/flying-dog-beers'),
+    html.Br(),
+    html.A('Data Source', href='https://www.flyingdog.com/beers/'),
+    ]
+)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
